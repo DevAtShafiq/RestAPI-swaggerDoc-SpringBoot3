47b9777b3b3d68426c721cec6fa0e53ca2204345
ISLAM MD SHAFIQUL <98973818+DevAtShafiq@users.noreply.github.com>
2024-05-02 12:05:14 +0900

    swagger and db updated

--- src/main/java/com/example/aroundstudiohub/Controller/PostController.java
+++ src/main/java/com/example/aroundstudiohub/Controller/PostController.java
@@ -1,7 +1,32 @@
 package com.example.aroundstudiohub.Controller;
 
+import com.example.aroundstudiohub.Dao.ProductDAOImpl;
+import com.example.aroundstudiohub.Entity.ProductEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/product")
+
 public class PostController {
+
+    final
+    ProductDAOImpl productDAO;
+
+    @Autowired
+    public PostController(ProductDAOImpl productDAO) {
+        this.productDAO = productDAO;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveToDb(@RequestBody ProductEntity productEntity) {
+
+        ProductEntity productEntity1 = productDAO.saveProduct(productEntity);
+
+        return ResponseEntity.status(200).body("successfully inserted"+" "+productEntity1.getProductId());
+    }
 }
