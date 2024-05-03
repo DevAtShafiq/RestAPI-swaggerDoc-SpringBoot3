dfe3174737bcfb0185ff348221a8d8d9c9a09ca3
ISLAM MD SHAFIQUL <98973818+DevAtShafiq@users.noreply.github.com>
2024-05-03 12:15:38 +0900

    Logger Implementing
    Logger controller post

--- src/main/java/com/example/aroundstudiohub/Controller/HelloController.java
+++ src/main/java/com/example/aroundstudiohub/Controller/HelloController.java
@@ -1,7 +1,26 @@
 package com.example.aroundstudiohub.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1")
 public class HelloController {
+
+
+
+    final Logger logger= LoggerFactory.getLogger(HelloController.class);
+
+    @PostMapping("/log-test")
+    public void logTest(){
+        logger.info("Info log");
+        logger.warn("warn log");
+        logger.error("error log");
+        logger.trace("trace log");
+        logger.debug("debug log");
+
+    }
 }
