59cd351b61c68efe19cd20b08bf524d5f8814aae
DevAtShafiq <98973818+DevAtShafiq@users.noreply.github.com>
2024-05-04 13:35:28 +0900

    a
    a

--- src/main/java/com/example/aroundstudiohub/Controller/PostController.java
+++ src/main/java/com/example/aroundstudiohub/Controller/PostController.java
@@ -1,12 +1,15 @@
 package com.example.aroundstudiohub.Controller;
 
 import com.example.aroundstudiohub.Dao.ProductDAOImpl;
-import com.example.aroundstudiohub.Dto.ProductDTO;
 import com.example.aroundstudiohub.Entity.ProductEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -31,9 +34,18 @@ public class PostController {
 
 
     @GetMapping("/show/{productId}")
-    public ResponseEntity<?> getProduct(@PathVariable String productId){
+    public Optional<ProductEntity> getProduct(@PathVariable String productId){
 
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(productDAO.getProduct(productId));
+
+        long  millisecond = System.currentTimeMillis();
+
+
+         Logger logger = LoggerFactory.getLogger(PostController.class);
+        logger.info("[ProductController] perform{} of Around Hub API.","getProduct");
+  
+        Optional<ProductEntity> productEntity =productDAO.getProduct(productId);
+
+    return productEntity;
     }
 
 }
