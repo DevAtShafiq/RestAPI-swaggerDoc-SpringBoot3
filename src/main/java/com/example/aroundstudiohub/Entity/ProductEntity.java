63f387f563aa93acf3ac3c9c41986775f6339a79
ISLAM MD SHAFIQUL <98973818+DevAtShafiq@users.noreply.github.com>
2024-05-08 13:42:48 +0900

    A
    A

--- src/main/java/com/example/aroundstudiohub/Entity/ProductEntity.java
+++ src/main/java/com/example/aroundstudiohub/Entity/ProductEntity.java
@@ -2,6 +2,9 @@ package com.example.aroundstudiohub.Entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -13,9 +16,19 @@ import lombok.*;
 @Table(name = "product")
 public class ProductEntity {
     @Id
+    @NotNull
     private String productId;
+
+    @NotNull
     private String productName;
+
+    @NotNull
+    @Min(800)//won
+    @Max(1000)//won
     private int productPrice;
+    @NotNull
+    @Min(0)//won
+    @Max(1000)//won
     private int productStock;
 
 
